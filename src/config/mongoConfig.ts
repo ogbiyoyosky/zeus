0941ff4e7fb05465e6b3d3def968abecbf4dc9f8
Emmanuel Ogbiyoyo <emmanuel.o@crennettechlabs.com>
2020-10-05 09:32:15 +0100

    change mongo config

--- src/config/mongoConfig.ts
+++ src/config/mongoConfig.ts
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
-const mongo_uri =
-  process.env.NODE_ENV === "development"
-    ? process.env.MONGO_URI
-    : process.env.TEST_MONGO_URI;
+const mongo_uri 
+
+if(process.env.NODE_ENV != "test") {
+  mongo_uri = process.env.MONGO_URI
+} else {
+  mongo_uri = process.env.TEST_MONGO_URI
+}
 
 const mongoConfig = {
   mongo_uri,
