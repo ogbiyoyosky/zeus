04f2e143ed7bb6e780551a68a119bb370e10c7cc
Sir Freeman <emmanuel.o@crenettechlabs.com>
2020-10-05 11:29:01 +0100

    Add restructure on application

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -3,6 +3,8 @@ require("dotenv").config();
 const redisConfig = {
   port: parseInt(process.env.REDIS_PORT, 10),
   host: process.env.REDIS_HOST,
+  url: process.env.REDIS_URL,
+  password: process.env.REDIS_PASSWORD,
 };
 
 export default redisConfig;
